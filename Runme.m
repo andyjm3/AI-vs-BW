fff044dba90075d6f05bafe91240e6016230ec39
Andi Han <68631477+andyjm3@users.noreply.github.com>
2021-06-01 10:14:19 +1000

    Create Runme.m

---
+++ Runme.m
@@ -0,0 +1,27 @@
+addpath(pwd);
+
+cd thirdpartytools;
+addpath(genpath(pwd));
+cd ..;
+
+cd auxiliary/;
+addpath(genpath(pwd));
+cd ..;
+
+cd thirdpartytools;
+addpath(genpath(pwd));
+cd ..;
+
+cd tests;
+addpath(genpath(pwd));
+cd ..;
+
+% Experiments
+% weighted least squares
+test_linear_loss()
+  
+% Lyapunov equation
+%test_lyapunov_loss()
+  
+% trace regression
+%test_trace_regression()
